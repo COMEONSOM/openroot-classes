9ba49e0fd880928c3c2ede35c0d478e33abb13fe
Somnath Banerjee <aryansom80@gmail.com>
2025-11-22 12:26:07 +0530

    updated hsting

--- src/firebase.js
+++ src/firebase.js
@@ -5,24 +5,20 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-// manforceutube
+// sb8787878
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBUYRpjY6F5sBAOuFhFFfApZzbNiIZZsUk",
-  authDomain: "xfactorialdi.firebaseapp.com",
-  projectId: "xfactorialdi",
-  storageBucket: "xfactorialdi.firebasestorage.app",
-  messagingSenderId: "644930268242",
-  appId: "1:644930268242:web:a027e13c458947ad6fc38b",
-  measurementId: "G-VPX5PBC11J"
+  apiKey: "AIzaSyASevnap1cBLWdsOFo5Vt2_Vem2ezMOGfc",
+  authDomain: "openroot-classes-firebase.firebaseapp.com",
+  projectId: "openroot-classes-firebase",
+  storageBucket: "openroot-classes-firebase.firebasestorage.app",
+  messagingSenderId: "21969607390",
+  appId: "1:21969607390:web:27899989e5b1b057b1804f",
+  measurementId: "G-1DYLYQDBSR"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-export { auth, provider, signInWithPopup, signOut };
