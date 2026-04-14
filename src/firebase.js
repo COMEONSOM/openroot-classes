488b9ffcf8ed469d70ef65fc2c48546d08933b1f
Somnath Banerjee <aryansom80@gmail.com>
2026-04-14 14:41:02 +0530

    updated variables

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASevnap1cBLWdsOFo5Vt2_Vem2ezMOGfc",
-  authDomain: "openroot-classes-firebase.firebaseapp.com",
-  projectId: "openroot-classes-firebase",
-  storageBucket: "openroot-classes-firebase.firebasestorage.app",
-  messagingSenderId: "21969607390",
-  appId: "1:21969607390:web:27899989e5b1b057b1804f",
-  measurementId: "G-1DYLYQDBSR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
